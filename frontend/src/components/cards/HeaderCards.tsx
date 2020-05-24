50cb68b43637fb3a156bd411bd1e030b7dc0ad51
westergaards <steve.westergaard@gmail.com>
2020-05-24 10:12:21 -0700

    update padding for today totals

--- frontend/src/components/cards/HeaderCards.tsx
+++ frontend/src/components/cards/HeaderCards.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
-import { Box, Typography, Grid } from "@material-ui/core";
+import { Box, Typography, Grid, makeStyles } from "@material-ui/core";
 import { useMount } from "react-use";
 import { format } from "date-fns";
 import { Card } from "./Card";
 import { getCountrySummaryStatsToday } from "../../utils/novelApi";
 import { Country } from "../../utils/interfaces/country.model";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      // backgroundColor: theme.palette.secondary.main,
+    },
+    [theme.breakpoints.up("md")]: {
+      // backgroundColor: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up("lg")]: {
+      // backgroundColor: "#ececec",
+    },
+  },
+  charts: {
+    paddingTop: theme.spacing(2),
+  },
+}));
+
 export const HeaderCards = (props: any) => {
   const [value, setValue] = useState<Country>();
+  const classes = useStyles();
 
   useMount(async () => {
     const result = await getCountrySummaryStatsToday();
@@ -49,11 +68,11 @@ export const HeaderCards = (props: any) => {
         </Grid>
       </Grid>
 
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={6} sm={6} xl={3}>
+      <Grid container spacing={3} justify="center" className={classes.charts}>
+        <Grid item xs={6} sm={3} md={3} lg={3} xl={3}>
           <Card title="Today Deaths" value={value?.todayDeaths || 0} />
         </Grid>
-        <Grid item xs={6} sm={6} xl={3}>
+        <Grid item xs={6} sm={3} md={3} lg={3} xl={3}>
           <Card title="Today Cases" value={value?.todayCases || 0} />
         </Grid>
       </Grid>
